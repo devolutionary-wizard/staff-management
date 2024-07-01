c4718e50d8eae66c1fb40c942bc3207d64edcbd1
SEANGLAY <devolutionary.wizard@gmail.com>
2024-07-01 22:04:20 +0700

    chore: Add id column to staff table in init.sql

--- docker/mariadb/init.sql
+++ docker/mariadb/init.sql
@@ -1,9 +1,11 @@
 CREATE TABLE staff (
+    id INT NOT NULL AUTO_INCREMENT,
     staff_id VARCHAR(8) NOT NULL,
     full_name VARCHAR(100) NOT NULL,
     birthday DATE,
     gender VARCHAR(10),
-    PRIMARY KEY (staff_id)
+    PRIMARY KEY (id),
+    UNIQUE (staff_id)
 );
 
 ALTER TABLE staff ADD CONSTRAINT chk_gender CHECK (gender IN ('male', 'female') OR gender IS NULL);
