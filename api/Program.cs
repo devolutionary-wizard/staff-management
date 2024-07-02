ab3ec4f56d2de7dfcc392daf11a5a0d01f62c8b5
SEANGLAY <devolutionary.wizard@gmail.com>
2024-07-02 22:34:37 +0700

    refactor: custom port

--- api/Program.cs
+++ api/Program.cs
@@ -1,17 +1,26 @@
+using dotenv.net;
+
 namespace Api
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            DotEnv.Load();
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-                Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT");
+            return Host.CreateDefaultBuilder(args)
                     .ConfigureWebHostDefaults(webBuilder =>
                     {
-                        webBuilder.UseStartup<Startup>();
+                        var url = $"http://localhost:{port}";
+                        webBuilder.UseStartup<Startup>()
+                        .UseUrls(url);
                     });
+        }
+
     }
 }
